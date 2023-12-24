59becdd334f96bedf251b0880b22d5384346c8af
ayato-tk <lucasmafilzo@gmail.com>
2023-12-24 20:18:40 -0300

    feat: Add validations

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,11 +1,35 @@
+using CleanArchMvc.Domain.Validation;
+
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
 
-        public string Name { get; set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id Value");
+            Id = id;
+            ValidateDomain(name);
+        }
 
         public ICollection<Product> Products { get; set; }
+
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.when(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+
+            DomainExceptionValidation.when(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
     }
 }
--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -1,21 +1,21 @@
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Product
+    public sealed class Product
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
 
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
-        public string Description { get; set; }
+        public string Description { get; private set; }
 
-        public decimal Price { get; set; }
+        public decimal Price { get; private set; }
 
-        public int Stock { get; set; }
+        public int Stock { get; private set; }
 
-        public string Image { get; set; }
+        public string Image { get; private set; }
 
-        public int CategoryId { get; set; }
+        public int CategoryId { get; private  set; }
 
-        public Category Category { get; set; }   
+        public Category Category { get; private set; }   
     }
 }
