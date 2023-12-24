8a9ecf91423f97269f62f57f0bf19a3c436da23f
ayato-tk <lucasmafilzo@gmail.com>
2023-12-24 20:06:00 -0300

    feat: Add initial files

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
