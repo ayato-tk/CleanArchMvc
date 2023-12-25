e21bbd93f69bc0a5fa0397bb67104f920de2e237
ayato-tk <lucasmafilzo@gmail.com>
2023-12-25 15:40:56 -0300

    feat: Add dependency injection, mysql database with docker image and initial migrations

---
+++ CleanArchMvc.Infra.Data/Migrations/20231225183402_SeedProducts.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                   "VALUES('DVD', 'Aparelho DVD', 7.45, 50, 'caderno1.jpg', 1)");
+            mb.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId) " +
+                   "VALUES('Mouse', 'Um mouse para computador', 20.45, 50, 'mouse1.jpg', 2)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.IoC/README.md
@@ -0,0 +1,9 @@
+
+# APENAS PARA SALVAR
+
+
+Transient = Cria os objetos sempre que for solicitado
+
+Scoped = Cria os objetos uma vez por solicitação
+
+Singleton = Cria os objetos apenas na primeira vez que for solicitado
